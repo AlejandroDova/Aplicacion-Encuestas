356774da8a6296949a272b5c86a059c746b0feb3
Dova <alejandrodeve04@gmail.com>
2019-06-07 10:49:18 -0600

    Creacion de adaptador para la lista de preguntas.

--- app/src/main/java/com/example/globalcomapp/ADDCuestionario.java
+++ app/src/main/java/com/example/globalcomapp/ADDCuestionario.java
@@ -1,9 +1,12 @@
 package com.example.globalcomapp;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.globalcomapp.Componentes.Pregunta;
 
@@ -13,11 +16,15 @@ public class ADDCuestionario extends AppCompatActivity {
 
     ArrayList<Pregunta> listPregunta;
     RecyclerView recyclerPregunta;
+    Button agregarVF,agregarPOM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcuestionario);
+
+        agregarPOM = findViewById(R.id.buttonPOM);
+        agregarVF = findViewById(R.id.buttonPVF);
 
         listPregunta = new ArrayList<>();
         recyclerPregunta = findViewById(R.id.recyclerid);
@@ -25,5 +32,24 @@ public class ADDCuestionario extends AppCompatActivity {
 
         MyAdapterPreguntas myAdapterPreguntas = new MyAdapterPreguntas(listPregunta);
         recyclerPregunta.setAdapter(myAdapterPreguntas);
+
+
+
+        agregarPOM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final AlertDialog.Builder alertPOM = new AlertDialog.Builder(ADDCuestionario.this);
+                View preguntaview = getLayoutInflater().inflate(R.layout.dialog_preguntas,null);
+                alertPOM.setTitle("titulo");
+                alertPOM.setView(preguntaview);
+
+                AlertDialog alertDialog = alertPOM.create();
+                alertDialog.show();
+
+            }
+        });
+
+
     }
 }
