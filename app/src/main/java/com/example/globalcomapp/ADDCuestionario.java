5005a9ba1db42dc1d9227b9cd5936e0c52557597
Dova <alejandrodeve04@gmail.com>
2019-06-10 09:25:17 -0600

    Inicio conexion db remota.

--- app/src/main/java/com/example/globalcomapp/ADDCuestionario.java
+++ app/src/main/java/com/example/globalcomapp/ADDCuestionario.java
@@ -1,5 +1,6 @@
 package com.example.globalcomapp;
 
+import android.net.nsd.NsdManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,11 +9,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.example.globalcomapp.Componentes.Pregunta;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class ADDCuestionario extends AppCompatActivity {
+public class ADDCuestionario extends AppCompatActivity implements Response.Listener<JSONObject>,Response.ErrorListener {
 
     ArrayList<Pregunta> listPregunta;
     RecyclerView recyclerPregunta;
@@ -41,7 +46,7 @@ public class ADDCuestionario extends AppCompatActivity {
 
                 final AlertDialog.Builder alertPOM = new AlertDialog.Builder(ADDCuestionario.this);
                 View preguntaview = getLayoutInflater().inflate(R.layout.dialog_preguntas,null);
-                alertPOM.setTitle("titulo");
+                alertPOM.setTitle("Preguntas multiples");
                 alertPOM.setView(preguntaview);
 
                 AlertDialog alertDialog = alertPOM.create();
@@ -50,6 +55,29 @@ public class ADDCuestionario extends AppCompatActivity {
             }
         });
 
+        agregarVF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final AlertDialog.Builder alertPOM = new AlertDialog.Builder(ADDCuestionario.this);
+                View preguntaview = getLayoutInflater().inflate(R.layout.dialog_preguntas,null);
+                    alertPOM.setTitle("Preguntas V/F");
+                alertPOM.setView(preguntaview);
+
+                AlertDialog alertDialog = alertPOM.create();
+                alertDialog.show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
 
     }
 }
